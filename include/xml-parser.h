93385745d7e02018d3544ed767a3e6e37f6be63b
Cors <orselcem@gmail.com>
2015-08-08 23:28:06 +0300

    xml parser functionality started.

---
+++ include/xml-parser.h
@@ -0,0 +1,11 @@
+#ifndef XML_PARSER_H_INCLUDED
+#define XML_PARSER_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include "rapidxml.hpp"
+
+void parseXml(const char *argFileName);
+
+#endif //XML_PARSER_H_INCLUDED
+
--- src/trn.cpp
+++ src/trn.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <GL/freeglut.h>
 #include "glut-functions.h"
+#include "xml-parser.h"
 
 int main(int argc, char **argv)
 {
+/*
 	TrnGlutFunctions::init(&argc, argv);
 	activeLevel = new Level();
 	glutMainLoop();
+*/
+	parseXml("data/test-file");
+	std::cout << "\nfin.\n";
 	return 0;
 }
